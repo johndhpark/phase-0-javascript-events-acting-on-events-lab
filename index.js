854e5a0f8d860f8de84b8b1872a6c1f90336e622
John DH. Park <john.dh.park@me.com>
2023-04-09 10:56:13 -0700

    completed javascript-events-acting-on-events-lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+	const leftNumbers = dodger.style.left.replace("px", "");
+	const left = parseInt(leftNumbers, 10);
+
+	if (left > 0) {
+		dodger.style.left = `${left - 1}px`;
+	}
+}
+
+function moveDodgerRight() {
+	const leftNumbers = dodger.style.left.replace("px", "");
+	const leftDodger = parseInt(leftNumbers, 10);
+
+	const field = document.getElementById("game");
+	const widthField = field.clientWidth;
+
+	if (leftDodger + 40 < widthField) {
+		dodger.style.left = `${leftDodger + 1}px`;
+	}
+}
+
+document.addEventListener("keydown", function (e) {
+	if (e.key === "ArrowLeft") {
+		moveDodgerLeft();
+	} else if (e.key === "ArrowRight") {
+		moveDodgerRight();
+	}
+});
